12360d52fdae030ff2aec21d7714963265a608b0
CuukyOfficial <just.cookie.jc@gmail.com>
2022-10-23 11:20:18 +0200

    Made Server extend PacketIOComponent

--- src/main/java/de/lama/packets/server/Server.java
+++ src/main/java/de/lama/packets/server/Server.java
@@ -1,13 +1,13 @@
 package de.lama.packets.server;
 
-import de.lama.packets.client.Client;
 import de.lama.packets.Packet;
-import de.lama.packets.event.EventHandlerContainer;
+import de.lama.packets.PacketIOComponent;
+import de.lama.packets.client.Client;
 import de.lama.packets.operation.Operation;
 
 import java.util.stream.Stream;
 
-public interface Server extends EventHandlerContainer {
+public interface Server extends PacketIOComponent {
 
     Operation open();
 
