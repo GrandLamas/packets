ae472b9fbdd666ad26b453532a549fe0548604cb
CuukyOfficial <just.cookie.jc@gmail.com>
2023-01-17 15:24:33 +0100

    Made records possible

--- src/main/java/de/lama/packets/HandshakePacket.java
+++ src/main/java/de/lama/packets/HandshakePacket.java
@@ -1,16 +1,7 @@
 package de.lama.packets;
 
-public class HandshakePacket implements Packet {
+public record HandshakePacket(String version) implements Packet {
 
     public static final long ID = 69420;
 
-    private final String version;
-
-    public HandshakePacket(String version) {
-        this.version = version;
-    }
-
-    public String getVersion() {
-        return this.version;
-    }
 }
